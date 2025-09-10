5111d7314694fa0dae74a9ba3affdc42951bb1a3
Muhammad Ibaadi Ilmi <muhammad.ibaadi@ui.ac.id>
2025-09-10 09:08:30 +0700

    Add app name, student name, and class to main page

--- main/views.py
+++ main/views.py
@@ -3,5 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 
 def show_main_page(request):
-    context = {}
+    context = {
+        'name'  : 'Muhammad Ibaadi Ilmi',
+        'class' : 'PBP A',
+    }
     return render(request, "index.html", context)
