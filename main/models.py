0b3d717c5c66928a65eecb828c3841af35396d28
Muhammad Ibaadi Ilmi <muhammad.ibaadi@ui.ac.id>
2025-09-10 08:59:38 +0700

    Create Product model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    name = models.CharField()
+    price = models.IntegerField()
+    description = models.TextField()
+    thumbnail = models.URLField()
+    category = models.CharField()
+    is_featured = models.BooleanField()
