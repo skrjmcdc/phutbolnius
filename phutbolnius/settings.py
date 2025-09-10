0fba48b5ba3a5e979ca565f683f62f7c4c70897d
Muhammad Ibaadi Ilmi <muhammad.ibaadi@ui.ac.id>
2025-09-10 09:54:04 +0700

    Add production mode

--- phutbolnius/settings.py
+++ phutbolnius/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
+import os
+from dotenv import load_dotenv
+# Load environment variables (including PRODUCTION)
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -22,10 +27,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-r#si7_!sn!t=x+1$(5w-j*b91xjhl0fv_x%_4s8rm$x!r5y+88'
 
+PRODUCTION = os.getenv('PRODUCTION', default='false').lower() == 'true'
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['muhammad-ibaadi-phutbolnius.pbp.cs.ui.ac.id']
+ALLOWED_HOSTS = ['localhost', 'muhammad-ibaadi-phutbolnius.pbp.cs.ui.ac.id']
 
 
 # Application definition
@@ -72,13 +79,28 @@ WSGI_APPLICATION = 'phutbolnius.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if PRODUCTION:
+    # Use credentials stored in PWS
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.getenv('DB_NAME'),
+            'USER': os.getenv('DB_USER'),
+            'PASSWORD': os.getenv('DB_PASSWORD'),
+            'HOST': os.getenv('DB_HOST'),
+            'PORT': os.getenv('DB_PORT'),
+            'OPTIONS': {
+                'options': f"-c search_path={os.getenv('SCHEMA', 'public')}"
+            }
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
