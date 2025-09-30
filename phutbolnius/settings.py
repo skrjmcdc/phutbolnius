0068598104e13e03140b73a8c1a6ca945e18bed3
Muhammad Ibaadi Ilmi <muhammad.ibaadi@ui.ac.id>
2025-09-30 14:00:44 +0700

    Add stylesheet

--- phutbolnius/settings.py
+++ phutbolnius/settings.py
@@ -142,6 +142,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+if DEBUG:
+    STATICFILES_DIRS = [
+        BASE_DIR / 'static'
+    ]
+else:
+    STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
